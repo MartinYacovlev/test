276a724ec2b5ec9d5095fa1b4eb392efc48ba17f
Martin <92585539+MartinYacovlev@users.noreply.github.com>
2021-12-09 09:52:26 +0300

    Re name #5

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ while '19' in s or '299' in s or '3999' in s:
     s = s.replace('19', '2', 1)
     s = s.replace('299', '3', 1)
     s = s.replace('3999', '1', 1)
-print('Ответ на 4 задание:', s, sep=" ")
+print('Ответ на 5 задание:', s, sep=" ")
